eee1eb8f5eb134cbc9faf00969ec4073d86502d8
Filippo <filippo.malgarini@truelayer.com>
2026-02-07 00:47:57 +0100

    Implemented google login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@/contexts/AuthContext";
 import Index from "./pages/Index";
 import Game from "./pages/Game";
 import Score from "./pages/Score";
@@ -30,35 +31,37 @@ const App = () => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/how-to-play" element={<HowToPlay />} />
-            <Route
-              path="/game/1"
-              element={<Game level={1} scores={scores} setScores={setScores} apiKey={apiKey} setApiKey={handleSetApiKey} />}
-            />
-            <Route
-              path="/game/2"
-              element={<Game level={2} scores={scores} setScores={setScores} apiKey={apiKey} setApiKey={handleSetApiKey} />}
-            />
-            <Route
-              path="/game/3"
-              element={<Game level={3} scores={scores} setScores={setScores} apiKey={apiKey} setApiKey={handleSetApiKey} />}
-            />
-            <Route path="/score" element={<Score scores={scores} resetGame={resetGame} />} />
-            <Route 
-              path="/distance" 
-              element={<Distance apiKey={apiKey} />} 
-            />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
+      <AuthProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/how-to-play" element={<HowToPlay />} />
+              <Route
+                path="/game/1"
+                element={<Game level={1} scores={scores} setScores={setScores} apiKey={apiKey} setApiKey={handleSetApiKey} />}
+              />
+              <Route
+                path="/game/2"
+                element={<Game level={2} scores={scores} setScores={setScores} apiKey={apiKey} setApiKey={handleSetApiKey} />}
+              />
+              <Route
+                path="/game/3"
+                element={<Game level={3} scores={scores} setScores={setScores} apiKey={apiKey} setApiKey={handleSetApiKey} />}
+              />
+              <Route path="/score" element={<Score scores={scores} resetGame={resetGame} />} />
+              <Route
+                path="/distance"
+                element={<Distance apiKey={apiKey} />}
+              />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </AuthProvider>
     </QueryClientProvider>
   );
 };
